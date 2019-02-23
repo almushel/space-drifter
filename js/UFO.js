69368ef0ccc191ef7219ff4f67adb27524b19ae6
Andrew Mushel <almushel@gmail.com>
2019-02-23 17:52:41 -0600

    UFO now gradually turns shortest distance to new direction.

--- js/UFO.js
+++ js/UFO.js
@@ -2,6 +2,7 @@
 const UFO_SPEED = 1.9;
 const UFO_DIR_CHANGE_INTERVAL = 85;
 const UFO_COLLISION_RADIUS = 20;
+const UFO_TURN_PRECISION = 0.05;
 
 ufoClass.prototype = new movingWrapPositionClass();
 
@@ -16,6 +17,7 @@ function ufoClass() {
 	this.reset = function() {
 		this.superClassReset();
 		this.ang = 0;
+		this.targetAng = 0;
 		this.cyclesUntilDirectionChange = 0;
 		
 		this.x = Math.random()*canvas.width;
@@ -33,18 +35,33 @@ function ufoClass() {
 	this.superClassMove = this.move; 
 	this.move = function() {
 		this.superClassMove();
+		var angDeltaX = (this.x + Math.cos(this.targetAng)) - this.x;
+		var angDeltaY = (this.y + Math.sin(this.targetAng)) - this.y;
+		var turnAngDelta = angDeltaX*Math.sin(this.ang) - angDeltaY*Math.cos(this.ang);
 		
-		this.cyclesUntilDirectionChange -= deltaT;
-		if(this.cyclesUntilDirectionChange <= 0) {
-			this.ang = Math.random()*Math.PI*2.0;
+
+		if (turnAngDelta > -UFO_TURN_PRECISION && turnAngDelta < UFO_TURN_PRECISION) {
+			this.cyclesUntilDirectionChange -= deltaT;
+		} else {
+			if (turnAngDelta < 0) {
+				this.ang += deltaT * Math.PI/180;
+			} else if (turnAngDelta > 0) {
+				this.ang -= deltaT * Math.PI/180;
+			}
 			this.xv = Math.cos(this.ang) * UFO_SPEED;
 			this.yv = Math.sin(this.ang) * UFO_SPEED;
+		}
+		
+		if(this.cyclesUntilDirectionChange <= 0) {
+			this.targetAng = Math.random()*Math.PI*2.0;
 			this.cyclesUntilDirectionChange = UFO_DIR_CHANGE_INTERVAL;
 		}
 	  }
 	  
 	this.draw = function() {
 		drawBitmapCenteredAtLocationWithRotation(this.myBitmap, this.x, this.y, this.ang+picAngOffset);
+		//drawLine(this.x, this.y, this.x + Math.cos(this.ang) * 100, this.y + Math.sin(this.ang) * 100, 'yellow');
+		//drawLine(this.x, this.y, this.x + Math.cos(this.targetAng) * 100, this.y + Math.sin(this.targetAng) * 100, 'green');
 	}
 
 } // end of class
