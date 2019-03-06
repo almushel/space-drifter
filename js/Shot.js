15eae97cc9d138b8409b686059604d3addf6c867
Andrew Mushel <almushel@gmail.com>
2019-03-06 15:32:47 -0600

    Improved player ship sprite and shot color.

--- js/Shot.js
+++ js/Shot.js
@@ -46,7 +46,7 @@ function shotClass() {
 	  
 	this.draw = function() {
 		if(this.shotLife > 0) {
-			colorCircle(this.x, this.y, SHOT_DISPLAY_RADIUS, 'red');
+			colorCircle(this.x, this.y, SHOT_DISPLAY_RADIUS, 'aqua');
 		}
 	}
 
