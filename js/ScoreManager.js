ab00ed2ccfb49fefbf72beb4d2f7b1f34811688e
Andrew Mushel <almushel@gmail.com>
2019-04-22 12:38:59 -0500

    Chain timer now works as intended.

--- js/ScoreManager.js
+++ js/ScoreManager.js
@@ -8,10 +8,10 @@ var currentScore = 0,
     timerStart = null;
 
 function drawScore() {
+    updateChainTimer();
     colorAlignedText(10, 30, 'left', '20px Arial', 'white', 'Score: '+ currentScore);
     colorAlignedText(10, 60, 'left', '20px Arial', 'white', 'x'+ currentMultiplier);
     colorAlignedText(10, 90, 'left', '20px Arial', 'white', 'Chain: '+ currentChain);
-    updateChainTimer();
     colorAlignedText(10, 120, 'left', '20px Arial', 'white', 'Timer: '+ currentTimeCount);
 }
 
@@ -31,18 +31,20 @@ function updateChainTimer() {
         currentTimeCount = Math.round((SCORE_CHAIN_TIME - (performance.now() - timerStart))/1000);
     } else {
         currentTimeCount = 0;
+        currentChain = 0;
+        updateMultiplier();
     }
 }
 
 function updateMultiplier() {
-    if (currentChain > SCORE_MULTI_MILESTONES[3]) {
-        currentMultiplier = 5;
-    } else if (currentChain > SCORE_MULTI_MILESTONES[2]) {
-        currentMultiplier = 4;
-    } else if (currentChain > SCORE_MULTI_MILESTONES[1]) {
-        currentMultiplier = 3;
-    } else if (currentChain > SCORE_MULTI_MILESTONES[0]) {
-        currentMultiplier = 2;
+    for (var i = SCORE_MULTI_MILESTONES.length - 1; i >= 0; i--) {
+        if (currentChain >= SCORE_MULTI_MILESTONES[i]) {
+            currentMultiplier = i + 2;
+            break;
+        }
+        if (i == 0) {
+            currentMultiplier = 1;
+        }
     }
 }
 
@@ -52,4 +54,5 @@ function startChainTimer() {
     }
 
     timerStart = performance.now();
+    currentTimeCount = Math.round((SCORE_CHAIN_TIME - (performance.now() - timerStart))/1000);
 }
