b2ec2ffbec2d9632a74df7dec83d2cea80889836
Andrew Mushel <almushel@gmail.com>
2019-08-11 12:35:41 -0500

    More accurate music loop point with setTimeout

--- js/Audio.js
+++ js/Audio.js
@@ -1,5 +1,7 @@
 let musicLoop1, musicLoop2;
 let audioCtx, musicSource1, musicSource2, gainNode;
+let loop1Queued = false, 
+	loop2Queued = false;
 
 function setupAudio() {
 	musicLoop1 = loadMusicTrack('./audio/Space Drifter.mp3');
@@ -13,19 +15,31 @@ function setupAudio() {
 	//Start repeat on second track at loop to preserve tails
 	//Timeupdate doesn't fire frequently enough to precisely detect loop points
 	musicLoop1.ontimeupdate = function () {
-		//Current loop point is 1:46.666
-		if (musicLoop1.currentTime >= 106.666 && (musicLoop2.ended || musicLoop2.paused)) {
-			musicLoop2.currentTime = 0;
-			musicLoop2.play();
+		if (musicLoop1.currentTime >= 106.66666 - 1 && musicLoop1.currentTime < 106.66666 && loop2Queued == false) {
+
+			//Cue the loop to play with setTimeout for more precision
+			setTimeout(function(){
+				musicLoop2.currentTime = 0;
+				musicLoop2.play();
+				loop2Queued = false;
+			}, (106.666 - musicLoop1.currentTime) * 1000);
+		
+			loop2Queued = true;
 		}
 	}
 
 	//Alternate back to first track.
 	musicLoop2.ontimeupdate = function () {
-		//Current loop point is 1:46.666
-		if (musicLoop2.currentTime >= 106.666 && (musicLoop1.ended || musicLoop1.paused)) {
-			musicLoop1.currentTime = 0;
-			musicLoop1.play();
+		if (musicLoop2.currentTime >= 106.66666 - 1 && musicLoop2.currentTime < 106.66666 && loop1Queued == false) {
+			
+			//Cue the loop to play with setTimeout for more precision
+			setTimeout(function(){
+				musicLoop1.currentTime = 0;
+				musicLoop1.play();
+				loop1Queued = false;
+			}, (106.66666 - musicLoop2.currentTime) * 1000);
+		
+			loop1Queued = true;
 		}
 	}
 /*
@@ -43,7 +57,6 @@ function setupAudio() {
 function loadMusicTrack(path) {
 	let newAudio = new Audio(path);
 	newAudio.preload = 'auto';
-	//newAudio.loop = true;
 
 	return newAudio;
 }
