d1200d18df4689c06ba94ca43548371c893d0616
Andrew Mushel <almushel@gmail.com>
2019-09-29 22:42:21 -0500

    Refactored spawnWarp draw.

--- js/enemies/SpawnWarp.js
+++ js/enemies/SpawnWarp.js
@@ -1,70 +1,65 @@
 const ENEMY_WARP_SPEED = 26;
 
 class SpawnWarp extends WrapPosition {
-    constructor(x, y, target, decay) {
-        super();
-        this.x = x;
-        this.y = y;
-        this.target = target;
-        this.isDead = false;
-        this.opening = true;
-        if (target.sprite == undefined) {
-            this.maxRadius = target.collisionRadius;
-        } else {
-            this.maxRadius = target.sprite.width;
-        }
-        this.decayRate = this.maxRadius / decay;
-        this.radius = this.maxRadius;
-    }
+	constructor(x, y, target, decay) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.target = target;
+		this.isDead = false;
+		this.opening = true;
+		if (target.sprite == undefined) {
+			this.maxRadius = target.collisionRadius;
+		} else {
+			this.maxRadius = target.sprite.width;
+		}
+		this.decayRate = this.maxRadius / decay;
+		this.radius = this.maxRadius;
+	}
 
-    move() {
-        if (this.opening) {
-            this.radius -= deltaT * this.decayRate;
-            if (this.radius <= 0) {
-                this.opening = false;
-            }
-        } else {
-            this.radius += deltaT * this.decayRate;
-            if (this.radius >= this.maxRadius) {
-                this.die();
-            }
-        }
+	move() {
+		if (this.opening) {
+			this.radius -= deltaT * this.decayRate;
+			if (this.radius <= 0) {
+				this.opening = false;
+			}
+		} else {
+			this.radius += deltaT * this.decayRate;
+			if (this.radius >= this.maxRadius) {
+				this.die();
+			}
+		}
 
-    }
+	}
 
-    collide() {
-        return;
-    }
+	collide() {
+		return;
+	}
 
-    draw(){
-        if (!this.isDead) {
-            let sprite = this.target.sprite;
-            ctx.strokeStyle = '#6DC2FF';
-            ctx.lineWidth = 1;
-            colorCircle(this.x, this.y, this.maxRadius/1.5 - this.radius/1.5, '#000a30');
-            ctx.stroke();
+	draw() {
+		if (!this.isDead) {
+			ctx.strokeStyle = '#6DC2FF';
+			ctx.lineWidth = 1;
+			colorCircle(this.x, this.y, this.maxRadius / 1.5 - this.radius / 1.5, '#000a30');
+			ctx.stroke();
 
-            if (this.opening) {
-                return;
-            }
-            
-            if (sprite == undefined) {
-                ctx.save()
-                ctx.translate(this.x, this.y)
-                ctx.scale(this.radius/this.maxRadius, this.radius/this.maxRadius);
-                this.target.drawSprite(0, 0);
-                ctx.restore();
-            } else {
-                ctx.drawImage(sprite, 0, 0, sprite.width, sprite.width, this.x-this.radius/2, this.y-this.radius/2, this.radius, this.radius);
-            }
-        }
-    }
+			if (this.opening) {
+				return;
+			}
 
-    die() {
-        this.target.invulnerabilityTime = 0;
-        allEntities.push(this.target);
-        enemyList.push(this.target);
-        spawnFinished = true;
-        this.isDead = true;
-    }
+			ctx.save()
+			ctx.translate(this.x, this.y)
+			ctx.scale(this.radius / this.maxRadius, this.radius / this.maxRadius);
+			this.target.drawSprite(0, 0);
+			ctx.restore();
+		}
+	}
+
+	die() {
+		this.target.invulnerabilityTime = 0;
+		allEntities.push(this.target);
+		enemyList.push(this.target);
+		spawnFinished = true;
+		this.isDead = true;
+	}
 }
